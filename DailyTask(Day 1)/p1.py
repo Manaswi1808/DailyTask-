86428782e7acda07848ef2a8ce515bdc681f4008
Manaswi1808 <141210737+Manaswi1808@users.noreply.github.com>
2025-01-06 17:15:04 +0530

    Add files via upload

---
+++ DailyTask(Day 1)/p1.py
@@ -0,0 +1,11 @@
+# Define two numbers a and b
+a = 5
+b = 3
+
+# Calculate multiplication and sum
+multiplication = a * b
+sum = a + b
+
+# Print the results
+print("Multiplication of 2 numbers=",multiplication)
+print("Sum of 2 numbers=",sum)
---
+++ DailyTask(Day 1)/p2.py
@@ -0,0 +1,9 @@
+# Declare two variables num1 and num2
+num1 = 15
+num2 = 30
+
+# Use the ternary operator to assign num1 to largest if it's greater than num2, else assign num2.
+largest = num1 if num1 > num2 else num2
+
+# Print the largest number
+print("The largest number is=", largest)

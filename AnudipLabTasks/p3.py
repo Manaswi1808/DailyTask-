e2e5d4f1a6c2384f1419250dc7272c7176836b73
Manaswi1808 <141210737+Manaswi1808@users.noreply.github.com>
2025-01-06 17:06:03 +0530

    Add files via upload

---
+++ AnudipLabTasks/p3.py
@@ -0,0 +1,8 @@
+# Input temperature in Celsius
+celsius = float(input("Enter temperature in Celsius= "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Print the result
+print(f"{celsius}°C is equal to {fahrenheit}°F")

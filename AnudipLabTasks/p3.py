754b48409b6361bf1241f94e4ec625ae434b0f20
Manaswi1808 <141210737+Manaswi1808@users.noreply.github.com>
2025-01-06 17:14:25 +0530

    Delete AnudipLabTasks directory

--- AnudipLabTasks/p3.py
+++
@@ -1,8 +0,0 @@
-# Input temperature in Celsius
-celsius = float(input("Enter temperature in Celsius= "))
-
-# Convert Celsius to Fahrenheit
-fahrenheit = (celsius * 9/5) + 32
-
-# Print the result
-print(f"{celsius}°C is equal to {fahrenheit}°F")
--- AnudipLabTasks/p4.py
+++
@@ -1,9 +0,0 @@
-# Input the base and height of the triangle
-base = float(input("Enter the base of the triangle= "))
-height = float(input("Enter the height of the triangle= "))
-
-# Calculate the area of the triangle
-area = 0.5 * base * height
-
-# Print the area
-print("Area of Triangle=",area)
